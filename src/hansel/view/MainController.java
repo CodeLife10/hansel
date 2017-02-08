9385d20c29f83cb6c6d6312410370e6850532250
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-02-08 15:25:57 +0000

    directory explorer update and default to user folders

--- src/hansel/view/MainController.java
+++ src/hansel/view/MainController.java
@@ -5,19 +5,47 @@ import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.stage.*;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainController implements Initializable{
 
     Stage openDirectoryStage;
+    private Desktop desktop = Desktop.getDesktop();
 
     public MenuItem open_directory;
 
     public void openDirectory() {
         FileChooser fileChooser = new FileChooser();
+        configureFileChooser(fileChooser);
         fileChooser.setTitle("Open Directory");
-        fileChooser.showOpenDialog(openDirectoryStage);
+
+        File file = fileChooser.showOpenDialog(openDirectoryStage);
+        if(file != null) {
+            openFile(file);
+        }
+    }
+
+    private void openFile(File file) {
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(
+                    FileChooser.class.getName()).log(
+                    Level.SEVERE, null, ex
+            );
+        }
+    }
+
+    private static void configureFileChooser(final FileChooser fileChooser){
+        fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+        );
     }
 
 
