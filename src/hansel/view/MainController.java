a7bd63704165cb0762c33c236b8687c8b8f2eaa6
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-02-08 15:16:34 +0000

    initial directory explorer

--- src/hansel/view/MainController.java
+++ src/hansel/view/MainController.java
@@ -1,15 +1,28 @@
 package hansel.view;
 
 import javafx.fxml.Initializable;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable{
 
+    Stage openDirectoryStage;
+
+    public MenuItem open_directory;
+
+    public void openDirectory() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Directory");
+        fileChooser.showOpenDialog(openDirectoryStage);
+    }
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 }
